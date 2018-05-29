fb5e0ea842520949fe5c08d29bae57770cb3ebee
Dominykas <dominykas.raila@gmail.com>
2018-05-29 18:07:51 +0300

    Added feature to logout and created a dummy dashboard.

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
 <?php
 require('bootstrap.php');
-$action = 'login';
+session_start();
+var_dump([$_POST, $_GET], $_SESSION);
+if (array_key_exists('user',$_SESSION) && isset($_SESSION['user'])) {
+    $user = $entityManager->find(User::class, $_SESSION['user']);
+
+    if (! $user instanceof User) {
+        $_SESSION['user'] = null;
+        $action = 'login';
+    }
+
+    $action = 'dashboard';
+} else {
+    $action = 'login';
+}
+
 if (array_key_exists('action',$_POST)) {
     $action = $_POST['action'];
 } else if (array_key_exists('action',$_GET)) {
@@ -19,8 +33,8 @@ switch ($action) {
             $password = $_POST['password'];
             $user = $entityManager->getRepository('User')->authenticate($username, $password);
             if ($user instanceof User) {
-                session_start();
                 $_SESSION['user'] = $user->getId();
+                header("Location: index.php?action=dashboard");
             } else {
                 echo $twig->render('error.twig', [
                     'error_short' => "Invalid username or password.",
@@ -32,5 +46,15 @@ switch ($action) {
                 'error_long' => "Go back to index."
             ]);
         }
+        break;
+    case 'logout':
+        $_SESSION['user'] = null;
+        header("Location: index.php");
+        break;
+    case 'dashboard':
+        echo $twig->render('dashboard.twig', [
+            'title' => 'Dashboard',
+            'user' => $user
+        ]);
         break;
 }
