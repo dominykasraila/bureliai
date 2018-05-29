b69fd64dc1e6817561d3f1e8aeab7ac2c432ff32
Dominykas <dominykas.raila@gmail.com>
2018-05-29 17:29:45 +0300

    Moved login and authentication to single controler.

--- index.php
+++ index.php
@@ -1,6 +1,36 @@
 <?php
 require('bootstrap.php');
+$action = 'login';
+if (array_key_exists('action',$_POST)) {
+    $action = $_POST['action'];
+} else if (array_key_exists('action',$_GET)) {
+    $action = $_GET['action'];
+}
 
-echo $twig->render('login.twig', [
-	'title' => 'Login'
-]);
+switch ($action) {
+    case 'login':
+        echo $twig->render('login.twig', [
+            'title' => 'Login'
+        ]);
+        break;
+    case 'authenticate':
+        if (!empty($_POST)) {
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            $user = $entityManager->getRepository('User')->authenticate($username, $password);
+            if ($user instanceof User) {
+                session_start();
+                $_SESSION['user'] = $user->getId();
+            } else {
+                echo $twig->render('error.twig', [
+                    'error_short' => "Invalid username or password.",
+                ]);
+            }
+        } else {
+            echo $twig->render('error.twig', [
+                'error_short' => "Please login.",
+                'error_long' => "Go back to index."
+            ]);
+        }
+        break;
+}
