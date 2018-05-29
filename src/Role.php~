81cde4724b114bffa7ea310ee8d19831d3715b30
Dominykas <dominykas.raila@gmail.com>
2018-05-29 13:03:29 +0300

    Added username and password fields to User class.

--- src/Role.php~
+++ src/Role.php~
@@ -9,4 +9,38 @@ class Role {
 	 * @Column(type="string", length=50)
 	 */
 	private $name;
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name.
+     *
+     * @param string $name
+     *
+     * @return Role
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
 }
--- src/User.php
+++ src/User.php
@@ -5,18 +5,32 @@ class User {
 	 * @Id @GeneratedValue @Column(type="integer")
 	 */
 	private $id;
+
 	/**
-	 * @Column(type="string", length=50)
+	 * @Column(type="string", length=50, unique=TRUE)
 	 */
+	 private $username;
+
+ 	/**
+ 	 * @Column(type="string", length=50)
+ 	 */
+ 	private $password;
+
+ 	/**
+ 	 * @ManyToOne(targetEntity="Role")
+ 	 */
 	private $name;
+
 	/**
 	 * @Column(type="string", length=50)
 	 */
 	private $surname;
+
 	/**
 	 * @ManyToOne(targetEntity="Role")
 	 */
 	private $role;
+
 
     /**
      * Get id.
@@ -98,5 +112,53 @@ class User {
     public function getRole()
     {
         return $this->role;
+    }
+
+    /**
+     * Set username.
+     *
+     * @param string $username
+     *
+     * @return User
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username.
+     *
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set password.
+     *
+     * @param string $password
+     *
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password.
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
     }
 }
