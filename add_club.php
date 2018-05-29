977538d78e1d56707c0483e6a4a5175edd181b30
Dominykas <dominykas.raila@gmail.com>
2018-05-29 23:19:15 +0300

    Features available: list clubs and supervisors.

--- add_club.php
+++ add_club.php
@@ -2,17 +2,20 @@
 // php .\add_club.php name supervisor_name
 require('bootstrap.php');
 
-if($argc != 3){
+if ($argc != 2 && $argc != 3) {
 	echo "Usage:\n  php .\add_club.php name supervisor_name";
 	exit;
 }
 
-$supervisor = $entityManager->getRepository('User')->findOneBy([
-	'username' => $argv[2]
-]);
-
-$club = new Club();
-$club->setName($argv[1])
-	->setSupervisor($supervisor);
+if($argc > 1) {
+	$club = new Club();
+	$club->setName($argv[1]);
+}
+if ($argc == 3) {
+	$supervisor = $entityManager->getRepository('User')->findOneBy([
+		'username' => $argv[2]
+	]);
+	$club->setSupervisor($supervisor);
+}
 $entityManager->persist($club);
 $entityManager->flush();
--- index.php
+++ index.php
@@ -1,81 +1,101 @@
 <?php
+function redirect() {
+	echo $twig->render('error.twig', [
+		'error_short' => "You don't have permission for that.",
+		'error_long' => "Go back to index."
+	]);
+}
 require('bootstrap.php');
 session_start();
 $dev = true;
 
 if (array_key_exists('user',$_SESSION) && isset($_SESSION['user'])) {
-    $user = $entityManager->find(User::class, $_SESSION['user']);
+	$user = $entityManager->find(User::class, $_SESSION['user']);
 
-    if (! $user instanceof User) {
-        $_SESSION['user'] = null;
-        $action = 'login';
-    }
+	if (! $user instanceof User) {
+		$_SESSION['user'] = null;
+		$action = 'login';
+	}
 
-    $action = 'dashboard';
+	$action = 'dashboard';
 } else {
-    $action = 'login';
+	$action = 'login';
 }
 
 if (array_key_exists('action',$_POST)) {
-    $action = $_POST['action'];
+	$action = $_POST['action'];
 } else if (array_key_exists('action',$_GET)) {
-    $action = $_GET['action'];
+	$action = $_GET['action'];
 }
 if ($dev) var_dump($_POST, $_GET, $_SESSION, $action);
 switch ($action) {
-    case 'login':
-
-        $users = $dev ? $entityManager->getRepository('User')->findAll() : null;
-        echo $twig->render('login.twig', [
-            'title' => 'Login',
-            'users' => $users
-        ]);
-        break;
-    case 'authenticate':
-        if (!empty($_POST)) {
-            $username = $_POST['username'];
-            $password = $_POST['password'];
-            $user = $entityManager->getRepository('User')->authenticate($username, $password);
-            if ($user instanceof User) {
-                $_SESSION['user'] = $user->getId();
-                header("Location: index.php?action=dashboard");
-            } else {
-                echo $twig->render('error.twig', [
-                    'error_short' => "Invalid username or password.",
-                ]);
-            }
-        } else {
-            echo $twig->render('error.twig', [
-                'error_short' => "Please login.",
-                'error_long' => "Go back to index."
-            ]);
-        }
-        break;
-    case 'logout':
-        $_SESSION['user'] = null;
-        header("Location: index.php");
-        break;
-    case 'dashboard':
-        echo $twig->render('dashboard.twig', [
-            'title' => 'Dashboard',
-            'user' => $user
-        ]);
-        break;
-    case 'list_supervisors':
-        $userRepo = $entityManager->getRepository('User');
-        $userHasPermision = (bool) $userRepo->authorizeAs($_SESSION['user'], 'Admin');
-        if ($userHasPermision) {
-            $supervisors = $userRepo->getSupervisors();
-            if ($dev) var_dump($supervisors);
-            echo $twig->render('list_supervisors.twig', [
-                'title' => 'Supervisors',
-                'supervisors' => $supervisors,
-                'user' => $user
-            ]);
-        } else {
-            echo $twig->render('error.twig', [
-                'error_short' => "You don't have permission for that.",
-                'error_long' => "Go back to index."
-            ]);
-        }
+	case 'login':
+		$users = $dev ? $entityManager->getRepository('User')->findAll() : null;
+		echo $twig->render('login.twig', [
+			'title' => 'Login',
+			'users' => $users
+		]);
+		break;
+	case 'authenticate':
+		if (!empty($_POST)) {
+			$username = $_POST['username'];
+			$password = $_POST['password'];
+			$user = $entityManager->getRepository('User')->authenticate($username, $password);
+			if ($user instanceof User) {
+				$_SESSION['user'] = $user->getId();
+				header("Location: index.php?action=dashboard");
+			} else {
+				echo $twig->render('error.twig', [
+					'error_short' => "Invalid username or password.",
+				]);
+			}
+		} else {
+			echo $twig->render('error.twig', [
+				'error_short' => "Please login.",
+				'error_long' => "Go back to index."
+			]);
+		}
+		break;
+	case 'logout':
+		$_SESSION['user'] = null;
+		header("Location: index.php");
+		break;
+		case 'dashboard':
+		echo $twig->render('dashboard.twig', [
+			'title' => 'Dashboard',
+			'user' => $user
+		]);
+		break;
+	case 'list_supervisors':
+		$userRepo = $entityManager->getRepository('User');
+		$userHasPermision = (bool) $userRepo->authorizeAs($_SESSION['user'], 'Admin');
+		if ($userHasPermision) {
+			$supervisors = $userRepo->getSupervisors();
+			if ($dev) var_dump($supervisors);
+			echo $twig->render('list_supervisors.twig', [
+				'title' => 'Supervisors',
+				'supervisors' => $supervisors,
+			]);
+		} else {
+			echo $twig->render('error.twig', [
+				'error_short' => "You don't have permission for that.",
+				'error_long' => "Go back to index."
+			]);
+		}
+		break;
+	case 'list_clubs':
+		$userRepo = $entityManager->getRepository('User');
+		$userHasPermision = (bool) $userRepo->authorizeAs($_SESSION['user'], 'Admin');
+		if ($userHasPermision) {
+			$clubRepo = $entityManager->getRepository('Club');
+			$clubs = $clubRepo->findAll();
+			if ($dev) var_dump($clubs);
+			echo $twig->render('list_clubs.twig', [
+				'title' => 'Clubs',
+				'clubs' => $clubs,
+			]);
+		} else {
+			redirect();
+		}
+		break;
 }
