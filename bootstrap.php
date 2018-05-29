eff8c68fc25fa09a952fc042ac7877fcb798c38c
Dominykas <dominykas.raila@gmail.com>
2018-05-29 11:41:59 +0300

    Added .gitignore, project dependencies and configuration.

---
+++ bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+// bootstrap.php
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+require_once "vendor/autoload.php";
+
+// Create a simple "default" Doctrine ORM configuration for Annotations
+$isDevMode = true;
+$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/src"), $isDevMode);
+// Database configuration parameters
+$conn = array(
+    'driver' => 'pdo_sqlite',
+    'path' => __DIR__ . '/db.sqlite',
+);
+
+// obtaining the entity manager
+$entityManager = EntityManager::create($conn, $config);
+
+// Twig API
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader, array(
+    'cache' => 'cache',
+));
